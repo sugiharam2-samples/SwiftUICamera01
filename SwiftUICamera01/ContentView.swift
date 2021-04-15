0d4277f5ce4cbbe26b28de36c7e4c644c191e9c6
Mamoru Sugihara <sugiharam2@gmail.com>
2021-04-16 01:02:04 +0900

    Add CameraView using UIViewController

--- SwiftUICamera01/ContentView.swift
+++ SwiftUICamera01/ContentView.swift
@@ -6,12 +6,47 @@
 //
 
 import SwiftUI
+import AVFoundation
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+		CameraView()
+			.edgesIgnoringSafeArea(.all)
     }
+}
+
+struct CameraView: UIViewControllerRepresentable {
+	func makeUIViewController(context: Context) -> ViewController {
+		ViewController()
+	}
+
+	func updateUIViewController(_ uiViewController: ViewController, context: Context) {
+	}
+}
+
+class ViewController: UIViewController {
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		let session = AVCaptureSession()
+		session.sessionPreset = .photo
+
+		if let device = AVCaptureDevice.DiscoverySession(deviceTypes: [.builtInWideAngleCamera],
+													  mediaType: .video,
+													  position: .unspecified)
+			.devices.first(where: { $0.position == .front }),
+		   let input = try? AVCaptureDeviceInput(device: device) {
+			session.addInput(input)
+
+			let layer = AVCaptureVideoPreviewLayer(session: session)
+			layer.videoGravity = .resizeAspectFill
+			layer.connection?.videoOrientation = .portrait
+			layer.frame = view.frame
+			view.layer.insertSublayer(layer, at: 0)
+
+			session.startRunning()
+		}
+	}
 }
 
 struct ContentView_Previews: PreviewProvider {
