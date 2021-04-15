f669ce2b32932e83d9bf982bdd139275db6097de
Mamoru Sugihara <sugiharam2@gmail.com>
2021-04-15 23:12:23 +0900

    Initial Commit

---
+++ SwiftUICamera01/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUICamera01
+//
+//  Created by Mamoru Sugihara on 2021/04/15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUICamera01/SwiftUICamera01App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUICamera01App.swift
+//  SwiftUICamera01
+//
+//  Created by Mamoru Sugihara on 2021/04/15.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUICamera01App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
